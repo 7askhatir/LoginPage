e89149791bed7f0a4f47f52c64d0eff8529cf9d1
Askhatir khalid <57253021+7askhatir@users.noreply.github.com>
2022-04-30 06:37:15 +0000

    Update mail.php

--- pages/mail.php
+++ pages/mail.php
@@ -14,8 +14,8 @@ $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose deb
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'chrimrtah35@gmail.com';                     //SMTP username
-    $mail->Password   = 'simoanawlah';                               //SMTP password
+    $mail->Username   = '.com';                     //SMTP username
+    $mail->Password   = '********';                               //SMTP password
     $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
     $mail->Port       = 465;       
     $mail->isHTML(true);
